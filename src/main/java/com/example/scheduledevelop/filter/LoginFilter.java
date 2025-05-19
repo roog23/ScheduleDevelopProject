494939f97bed17b0583f0ea856b84ddc7e548b64
roog23 <roog2320@gmail.com>
2025-05-19 17:23:28 +0900

    필터 예외처리 수정

--- src/main/java/com/example/scheduledevelop/filter/LoginFilter.java
+++ src/main/java/com/example/scheduledevelop/filter/LoginFilter.java
@@ -1,7 +1,10 @@
 package com.example.scheduledevelop.filter;
 
+import com.example.scheduledevelop.exception.dto.ErrorResponseDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.util.PatternMatchUtils;
 
@@ -14,11 +17,20 @@ public class LoginFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
         String requestURI = httpRequest.getRequestURI();
+
         if(!isWhiteList(requestURI)) {
             HttpSession session = httpRequest.getSession(false);
             if(session == null || session.getAttribute("sessionKey") == null) {
-                throw new RuntimeException("로그인이 필요합니다.");
+                ObjectMapper objectMapper = new ObjectMapper();
+                ErrorResponseDto errorResponseDto = new ErrorResponseDto("401 UNAUTHORIZED", "로그인이 필요합니다.");
+
+                httpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                httpResponse.setContentType("application/json");
+                httpResponse.setCharacterEncoding("UTF-8");
+                httpResponse.getWriter().write(objectMapper.writeValueAsString(errorResponseDto));
+                return;
             }
         }
         chain.doFilter(request, response);
